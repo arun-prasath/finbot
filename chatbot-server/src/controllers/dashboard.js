8958cafd2d335bcc5b9cf59ee2349149a7b491a0
Arun Prasasth Jeganathan <a.jeganathan@globallogic.com>
2020-10-06 20:20:30 +0530

    Implement metrics APIs

--- chatbot-server/src/controllers/dashboard.js
+++ chatbot-server/src/controllers/dashboard.js
@@ -1,5 +1,6 @@
 'use strict';
 const users = require('../models/user');
+const chatHistory = require('../models/chatHistory');
 
 class Dashboard{
 
@@ -14,22 +15,58 @@ class Dashboard{
     /**
      * Engagement Rate = total users visiting the site VS no of users who has initiated the chat
      */
-    getEngagementRate(){
-        
+    async getEngagementRate(req, res) {
+        let engagedUsers = await chatHistory.find({ connectionStatus: 'engaged' }).select('userid');
+        let responseObj = {
+            users: []
+        };
+
+        engagedUsers.forEach(user => responseObj.users.push(user.userid))
+
+        let totalUserCount = await users.countDocuments();
+        responseObj.engagedUsersCount = engagedUsers.length;
+        responseObj.totalUsersCount = totalUserCount;
+        responseObj.engagementRate = (( engagedUsers.length * 100 ) / totalUserCount).toFixed(2) + '%';
+
+        res.status(200).json(responseObj);
     }
 
     /**
      * Drop Rate = total users initiated a chat VS no of users who has left in between
      */
-    getDropoffRate(){
+    async getDropoffRate(req, res) {
+        let droppedUsers = await chatHistory.find({ connectionStatus: 'dropped' }).select('userid');
+        let responseObj = {
+            users: []
+        };
 
+        droppedUsers.forEach(user => responseObj.users.push(user.userid))
+
+        let totalUserCount = await users.countDocuments();
+        responseObj.droppedUsersCount = droppedUsers.length;
+        responseObj.totalUsersCount = totalUserCount;
+        responseObj.droppedRate = (( droppedUsers.length * 100 ) / totalUserCount).toFixed(2) + '%';
+
+        res.status(200).json(responseObj);
     }
 
     /**
      * completion rate = total users initiated a chat VS no of users who has finished the thank you or goodbye intent
      */
-    getCompletionRate(){
+    async getCompletionRate(req, res) {
+        let completedUsers = await chatHistory.find({ connectionStatus: 'completed' }).select('userid');
+        let responseObj = {
+            users: []
+        };
 
+        completedUsers.forEach(user => responseObj.users.push(user.userid))
+
+        let totalUserCount = await users.countDocuments();
+        responseObj.completedUsersCount = completedUsers.length;
+        responseObj.totalUsersCount = totalUserCount;
+        responseObj.completedUsers = (( completedUsers.length * 100 ) / totalUserCount).toFixed(2) + '%';
+
+        res.status(200).json(responseObj);
     }
 
 }
