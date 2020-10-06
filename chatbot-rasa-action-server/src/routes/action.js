d3045acae8b1be85774dc48bfc4be23f57d5bfc3
Arun Prasasth Jeganathan <a.jeganathan@globallogic.com>
2020-10-06 10:55:04 +0530

    Remove action server module

--- chatbot-rasa-action-server/src/routes/action.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-const ActionController = require('../controllers/action');
-
-router.post('/', ActionController.action);
-
-module.exports = router;
