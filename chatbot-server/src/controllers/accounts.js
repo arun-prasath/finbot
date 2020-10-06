e0dcc3c5fd17ac9da0d2681ff4b53664bc9d4d70
Arun Prasasth Jeganathan <a.jeganathan@globallogic.com>
2020-10-06 12:08:18 +0530

    Action server: fetch balance from account

--- chatbot-server/src/controllers/accounts.js
+++ chatbot-server/src/controllers/accounts.js
@@ -1,12 +1,43 @@
 'use strict';
 const jwtAuth = require('../helpers/jwtAuth');
 
-class AccountsController{
+const { response } = require('express');
+const jwtAuth = require('../helpers/jwtAuth');
+const AccountModel = require('../models/accounts');
 
-    constructor(){}
+class AccountsController {
 
-    async getAccountBalance(req, res, next){
-        res.status(200).json({'foo' : 'bar'});
+    constructor() {}
+
+    async getAccountBalance(req, res, next) {
+        const token = req.body.sender_id;
+        let responseText;
+
+        try {
+            let auth = new jwtAuth();
+            let guestDetails = auth.decode(token);
+            let audience = 'http://localhost:' + process.env.APP_SERVER_PORT;
+            auth.setOptions('finchatbot', guestDetails.payload.userid, audience);
+            let result = auth.verifyToken(token);
+            let userid = result.userid;
+
+            let account = await AccountModel.findOne({userid});
+            let balance = account.balance;
+
+            responseText = 'You have $' + balance + ' in your account';
+        } catch (err) {
+            console.log(err.message);
+            responseText = 'Authentication failed. Refresh your browser';
+        }
+
+        res.status(200).json({
+            events: [{
+                event: "action"
+            }],
+            responses: [{
+                text: responseText
+            }]
+        });
     }
 }
 
