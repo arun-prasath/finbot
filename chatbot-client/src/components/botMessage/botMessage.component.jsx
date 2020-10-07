3113bfc92f71c112b6e9eb9a5ae33c2fc5d1e80d
Arun Prasasth Jeganathan <a.jeganathan@globallogic.com>
2020-10-08 02:10:05 +0530

    ui changes

--- chatbot-client/src/components/botMessage/botMessage.component.jsx
+++ chatbot-client/src/components/botMessage/botMessage.component.jsx
@@ -2,20 +2,12 @@ import React from 'react';
 import './botMessage.component.scss';
 
 export const BotMessage = props => (
-    <div className={`${props.context} messages`}>
-        <div className="row h-100">
-            <div className="col-sm-auto p-0 m-0">
-                <div className="avatar pl-2 animate__animated animate__fadeInUp animate__delay-1s">
-                    <img className="fin-bot-avatar" src="https://robohash.org/111?set=set3&size=75x75" alt="Bot Avatar"/>
-                </div>
-            </div>
-            <div className="col-lg-auto p-0 m-0">
-                <div className="row h-100">
-                    <div className="my-auto animate__animated animate__fadeInUp animate__delay-1s message">
-                        {props.message}
-                    </div>
-                </div>
-            </div>
+    <div className="media">
+        <div className="w-15 d-inline-block animate__animated animate__fadeInUp animate__delay-1s">
+            <img className="align-self-center" src="https://robohash.org/111?set=set3&size=75x75" alt="Bot Avatar"/>
+        </div>
+        <div className="w-75 d-inline-block pr-2 media-body animate__animated animate__fadeInUp animate__delay-1s finbot-msg">
+            {props.message}
         </div>
     </div>
 );
