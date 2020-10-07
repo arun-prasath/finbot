08636782cefd35d87dc97e790e0fcf1b2a139641
Arun Prasasth Jeganathan <a.jeganathan@globallogic.com>
2020-10-07 21:35:15 +0530

    fixing env vars

--- chatbot-client/src/services/passport.js
+++ chatbot-client/src/services/passport.js
@@ -18,7 +18,8 @@ export default class Passport {
 
     static generatenewToken = async () => {
         try{
-            let endpoint = 'http://'+host+':'+app_server_port+'/v1/jwt_auth/new_session';
+            const base_url = process.env.REACT_APP_API_URL || config.host+':'+config.app_server_port;
+            let endpoint = base_url+'/v1/jwt_auth/new_session';
             const response = await axios.get(endpoint,{
                 params: {
                     timezone : Intl.DateTimeFormat().resolvedOptions().timeZone
